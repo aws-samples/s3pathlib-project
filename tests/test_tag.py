2807caf3c74425bb629eecdb2f565f288ca3eebc
Sanhe <MacHu-GWU@users.noreply.github.com>
2022-11-13 15:44:24 -0500

    chore

--- tests/test_tag.py
+++ tests/test_tag.py
@@ -22,4 +22,3 @@ if __name__ == "__main__":
     from s3pathlib.tests import run_cov_test
 
     run_cov_test(__file__, module="s3pathlib.tag", open_browser=False)
-    # run_cov_test(__file__, module="s3pathlib.tag", open_browser=True)
