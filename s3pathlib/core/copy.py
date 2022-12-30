6da43bfbd6b434cfdd4955321c568311f1635e8b
Sanhe Hu <husanhe@gmail.com>
2022-12-30 11:38:57 -0500

    add metadata tags parameter

--- s3pathlib/core/copy.py
+++ s3pathlib/core/copy.py
@@ -6,6 +6,9 @@ Copy file from s3 to s3.
 
 import typing as T
 
+from .. import client as better_client
+from ..type import TagType, MetadataType
+
 from .resolve_s3_client import resolve_s3_client
 from ..aws import context
 
@@ -22,20 +25,26 @@ class CopyAPIMixin:
     def copy_file(
         self: "S3Path",
         dst: "S3Path",
+        metadata: T.Optional[MetadataType] = None,
+        tags: T.Optional[TagType] = None,
         overwrite: bool = False,
         bsm: T.Optional["BotoSesManager"] = None,
     ) -> dict:
         """
         Copy an S3 directory to a different S3 directory, including all
-        sub directory and files.
+        sub-directory and files.
 
         :param dst: copy to s3 object, it has to be an object
-        :param overwrite: if False, non of the file will be upload / overwritten
+        :param overwrite: if False, none of the file will be uploaded / overwritten
             if any of target s3 location already taken.
 
         :return: number of object are copied, 0 or 1.
 
         .. versionadded:: 1.0.1
+
+        .. versionchanged:: 1.3.1
+
+            add ``metadata`` and ``tags`` argument
         """
         self.ensure_object()
         dst.ensure_object()
@@ -44,10 +53,14 @@ class CopyAPIMixin:
         if overwrite is False:
             dst.ensure_not_exists(bsm=bsm)
         s3_client = resolve_s3_client(context, bsm)
-        return s3_client.copy_object(
-            Bucket=dst.bucket,
-            Key=dst.key,
-            CopySource={"Bucket": self.bucket, "Key": self.key},
+        return better_client.copy_object(
+            s3_client,
+            src_bucket=self.bucket,
+            src_key=self.key,
+            dst_bucket=dst.bucket,
+            dst_key=dst.key,
+            metadata=metadata,
+            tags=tags,
         )
 
     def copy_dir(
@@ -58,10 +71,10 @@ class CopyAPIMixin:
     ):
         """
         Copy an S3 directory to a different S3 directory, including all
-        sub directory and files.
+        sub-directory and files.
 
         :param dst: copy to s3 directory, it has to be a directory
-        :param overwrite: if False, non of the file will be upload / overwritten
+        :param overwrite: if False, none of the file will be uploaded / overwritten
             if any of target s3 location already taken.
 
         :return: number of objects are copied
@@ -90,6 +103,8 @@ class CopyAPIMixin:
     def copy_to(
         self: "S3Path",
         dst: "S3Path",
+        metadata: T.Optional[MetadataType] = None,
+        tags: T.Optional[TagType] = None,
         overwrite: bool = False,
         bsm: T.Optional["BotoSesManager"] = None,
     ) -> int:
@@ -101,6 +116,10 @@ class CopyAPIMixin:
             if any of target s3 location already taken.
 
         .. versionadded:: 1.0.1
+
+        .. versionchanged:: 1.3.1
+
+            add ``metadata`` and ``tags`` argument
         """
         if self.is_dir():
             return self.copy_dir(
@@ -113,6 +132,8 @@ class CopyAPIMixin:
                 dst=dst,
                 overwrite=overwrite,
                 bsm=bsm,
+                metadata=metadata,
+                tags=tags,
             )
             return 1
         else:  # pragma: no cover
@@ -121,6 +142,8 @@ class CopyAPIMixin:
     def move_to(
         self: "S3Path",
         dst: "S3Path",
+        metadata: T.Optional[MetadataType] = None,
+        tags: T.Optional[TagType] = None,
         overwrite: bool = False,
         bsm: T.Optional["BotoSesManager"] = None,
     ) -> int:
@@ -133,9 +156,15 @@ class CopyAPIMixin:
             if any of target s3 location already taken.
 
         .. versionadded:: 1.0.1
+
+        .. versionchanged:: 1.3.1
+
+            add ``metadata`` and ``tags`` argument
         """
         count = self.copy_to(
             dst=dst,
+            metadata=metadata,
+            tags=tags,
             overwrite=overwrite,
             bsm=bsm,
         )
