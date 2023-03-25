e6c4d66f655c730abdaa9a23dc6790ed65bec122
Sanhe Hu <husanhe@gmail.com>
2023-03-25 08:40:41 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,6 +52,8 @@ extensions = [
     'docfly.directives',
 ]
 
+rtd_sphinx_search_file_type = "un-minified"
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -83,7 +85,7 @@ release = package.__version__
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
