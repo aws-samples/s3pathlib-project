a3e9b99356e5c4a4703cb95c34f77deff031af61
Sanhe Hu <husanhe@gmail.com>
2023-04-16 14:58:08 -0400

    refactor test helpers

--- s3pathlib/tests/helpers.py
+++ s3pathlib/tests/helpers.py
@@ -3,7 +3,7 @@
 import sys
 import subprocess
 
-from .paths import dir_project_root, dir_htmlcov, bin_pytest
+from .paths import dir_project_root, dir_htmlcov, path_cov_index_html, bin_pytest
 
 if sys.platform == "win32":
     open_cmd = "open"
@@ -37,11 +37,14 @@ def _run_cov_test(
     module = normalize_module(module)
     args = [
         bin_pytest,
-        "-s", "--tb=native",
+        "-s",
+        "--tb=native",
         f"--rootdir={root_dir}",
         f"--cov={module}",
-        "--cov-report", "term-missing",
-        "--cov-report", f"html:{htmlcov_dir}",
+        "--cov-report",
+        "term-missing",
+        "--cov-report",
+        f"html:{htmlcov_dir}",
         script,
     ]
     subprocess.run(args)
@@ -56,22 +59,4 @@ def run_cov_test(script: str, module: str, preview: bool = False):
         htmlcov_dir=f"{dir_htmlcov}",
     )
     if preview:
-        _open_cov_report(module)
-
-
-def _open_cov_report(module: str):
-    module = normalize_module(module)
-    parts = module.split(".")
-    module_name = parts[-1]
-    module_path = str(dir_project_root.joinpath(*parts)) + ".py"
-    print(module_path)
-    found_cov_report_html = False
-    html_path = None
-    for p in dir_htmlcov.glob("**/*_py.html"):
-        if p.name.endswith(f"{module_name}_py.html"):
-            if module_path in p.read_text():
-                found_cov_report_html = True
-                html_path = str(p)
-                break
-    if found_cov_report_html:
-        subprocess.run([open_cmd, html_path])
+        subprocess.run(["open", f"{path_cov_index_html}"])
--- s3pathlib/tests/paths.py
+++ s3pathlib/tests/paths.py
@@ -8,6 +8,7 @@ dir_project_root = Path(__file__).absolute().parent.parent.parent
 # code structure
 dir_tests: Path = dir_project_root / "tests"
 dir_htmlcov: Path = dir_project_root / "htmlcov"
+path_cov_index_html = dir_htmlcov / "index.html"
 
 # virtual environment
 dir_bin: Path = Path(sys.executable).parent
