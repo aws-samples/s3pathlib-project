9122c1a23eb07ce17d06b60002f469147a4dad01
Sanhe Hu <husanhe@gmail.com>
2023-04-16 23:52:25 -0400

    Update test_core_exists.py

--- tests/core/test_core_exists.py
+++ tests/core/test_core_exists.py
@@ -3,46 +3,53 @@
 import pytest
 from s3pathlib.core import S3Path
 from s3pathlib import client as better_client
-from s3pathlib.tests import s3_client, bucket, prefix, run_cov_test
+from s3pathlib.tests import run_cov_test
+from s3pathlib.tests.mock import BaseTest
 
-s3dir_root = S3Path(bucket, prefix, "core", "exists/")
 
+class ExistsAPIMixin(BaseTest):
+    module = "core.exists"
 
-class TestExistsAPIMixin:
-    def test_exists(self):
+    def _test_exists(self):
         # s3 bucket
-        assert S3Path(bucket).exists() is True
+        assert S3Path(self.bucket).exists() is True
+
         # have access but not exists
-        assert S3Path(f"{bucket}-not-exists").exists() is False
+        assert S3Path(f"{self.bucket}-not-exists").exists() is False
+
         # doesn't have access
-        with pytest.raises(Exception):
-            assert S3Path("asdf").exists() is False
+        if self.use_mock is False:
+            with pytest.raises(Exception):
+                assert S3Path("asdf").exists() is False
 
         # s3 object
-        s3path_file = s3dir_root.joinpath("file.txt")
+        s3path_file = self.s3dir_root.joinpath("file.txt")
         better_client.put_object(
-            s3_client,
+            self.s3_client,
             s3path_file.bucket,
             s3path_file.key,
             b"a",
         )
         assert s3path_file.exists() is True
 
-        s3path_empty_object = s3dir_root.joinpath("empty.txt")
+        with pytest.raises(Exception):
+            s3path_file.ensure_not_exists()
+
+        s3path_empty_object = self.s3dir_root.joinpath("empty.txt")
         better_client.put_object(
-            s3_client,
+            self.s3_client,
             s3path_empty_object.bucket,
             s3path_empty_object.key,
             b"",
         )
         assert s3path_empty_object.exists() is True
 
-        assert S3Path(bucket, "this-never-gonna-exists.exe").exists() is False
+        assert S3Path(self.bucket, "this-never-gonna-exists.exe").exists() is False
 
         # s3 directory
         assert s3path_file.parent.exists() is True
 
-        assert S3Path(bucket, "this-never-gonna-exists/").exists() is False
+        assert S3Path(self.bucket, "this-never-gonna-exists/").exists() is False
 
         # void path
         p = S3Path()
@@ -55,9 +62,9 @@ class TestExistsAPIMixin:
             p.exists()
 
         # soft folder
-        s3path_soft_folder_file = s3dir_root.joinpath("soft_folder", "file.txt")
+        s3path_soft_folder_file = self.s3dir_root.joinpath("soft_folder", "file.txt")
         better_client.put_object(
-            s3_client,
+            self.s3_client,
             s3path_soft_folder_file.bucket,
             s3path_soft_folder_file.key,
             b"a",
@@ -66,16 +73,16 @@ class TestExistsAPIMixin:
         assert s3path_soft_folder_file.exists() is True
 
         # hard folder
-        dir_hard_folder = s3dir_root.joinpath("hard_folder/")
+        dir_hard_folder = self.s3dir_root.joinpath("hard_folder/")
         better_client.put_object(
-            s3_client,
+            self.s3_client,
             dir_hard_folder.bucket,
             dir_hard_folder.key,
             b"",
         )
-        s3path_hard_folder_file = s3dir_root.joinpath("hard_folder", "file.txt")
+        s3path_hard_folder_file = self.s3dir_root.joinpath("hard_folder", "file.txt")
         better_client.put_object(
-            s3_client,
+            self.s3_client,
             s3path_hard_folder_file.bucket,
             s3path_hard_folder_file.key,
             b"a",
@@ -84,14 +91,25 @@ class TestExistsAPIMixin:
         assert s3path_hard_folder_file.exists() is True
 
         # empty folder
-        dir_empty_folder = s3dir_root.joinpath("empty_folder/")
+        dir_empty_folder = self.s3dir_root.joinpath("empty_folder/")
         better_client.put_object(
-            s3_client,
+            self.s3_client,
             dir_empty_folder.bucket,
             dir_empty_folder.key,
             b"",
         )
         assert dir_empty_folder.exists() is True
+
+    def test(self):
+        self._test_exists()
+
+
+class Test(ExistsAPIMixin):
+    use_mock = False
+
+
+class TestWithVersioning(ExistsAPIMixin):
+    use_mock = True
 
 
 if __name__ == "__main__":
