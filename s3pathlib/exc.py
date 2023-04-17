85ec2f591bbf5d20d98ff4448049f034b9e44b48
Sanhe Hu <husanhe@gmail.com>
2023-04-17 02:05:33 -0400

    Update exc.py

--- s3pathlib/exc.py
+++ s3pathlib/exc.py
@@ -13,8 +13,7 @@ def ensure_one_and_only_one_not_none(**kwargs) -> None:
         raise ValueError
     if sum([v is not None for _, v in kwargs.items()]) != 1:
         raise ValueError(
-            f"one and only one of arguments from "
-            f"{list(kwargs)} can be not None!"
+            f"one and only one of arguments from " f"{list(kwargs)} can be not None!"
         )
 
 
@@ -25,17 +24,39 @@ def ensure_all_none(**kwargs) -> None:
     if len(kwargs) == 0:
         raise ValueError
     if sum([v is not None for _, v in kwargs.items()]) != 0:
-        raise ValueError(
-            f"arguments from {list(kwargs)} has to be all None!"
-        )
+        raise ValueError(f"arguments from {list(kwargs)} has to be all None!")
 
 
-class S3BucketNotExist(Exception):
-    pass
+class _UriRelatedError(Exception):
+    _tpl: str
+
+    @classmethod
+    def make(cls, uri: str):
+        return cls._tpl.format(uri=uri)
 
 
-class S3ObjectNotExist(Exception):
-    pass
+class S3BucketNotExist(_UriRelatedError):
+    _tpl = "S3 bucket {uri} does not exist!"
+
+
+class S3FolderNotExist(_UriRelatedError):
+    _tpl = "S3 folder {uri} does not exist!"
+
+
+class S3ObjectNotExist(_UriRelatedError):
+    _tpl = "S3 object {uri} does not exist!"
+
+
+class S3BucketAlreadyExist(_UriRelatedError):
+    _tpl = "S3 bucket {uri} already exist!"
+
+
+class S3FolderAlreadyExist(_UriRelatedError):
+    _tpl = "S3 folder {uri} already exist!"
+
+
+class S3ObjectAlreadyExist(_UriRelatedError):
+    _tpl = "S3 object {uri} already exist!"
 
 
 class S3PermissionDenied(Exception):
