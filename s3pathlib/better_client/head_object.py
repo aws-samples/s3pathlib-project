65a0e5341e7d45160be2796ac0c510e7e6457ff6
Sanhe Hu <husanhe@gmail.com>
2023-04-17 04:04:15 -0400

    Update head_object.py

--- s3pathlib/better_client/head_object.py
+++ s3pathlib/better_client/head_object.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 
+"""
+Improve the head_object_ API.
+
+.. _head_object: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html#S3.Client.head_object
+"""
+
 import typing as T
 from datetime import datetime
 
@@ -8,7 +14,7 @@ from func_args import NOTHING, resolve_kwargs
 
 from .. import exc
 
-if T.TYPE_CHECKING:
+if T.TYPE_CHECKING: # pragma: no cover
     from mypy_boto3_s3 import S3Client
     from mypy_boto3_s3.type_defs import HeadObjectOutputTypeDef
 
@@ -34,6 +40,14 @@ def head_object(
     """
     Wrapper of head_object_.
 
+    Example:
+
+        >>> response = head_object(s3_client, "my-bucket", "file.txt")
+        >>> if response is None:
+        ...     print("Object not found")
+        ... else:
+        ...     print(response["LastModified"])
+
     :param s3_client: See head_object_
     :param bucket: See head_object_
     :param key: See head_object_
@@ -52,7 +66,7 @@ def head_object(
     :param ignore_not_found: Default is ``False``; if ``True``, return ``None``
         when object is not found instead of raising an error.
 
-    .. _head_object: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html#S3.Client.head_object
+    :return: See head_object_
     """
     try:
         dct = s3_client.head_object(
@@ -79,6 +93,41 @@ def head_object(
             if ignore_not_found:
                 return None
             else:
-                raise exc.S3ObjectNotExist.make(str(e))
+                raise exc.S3ObjectNotExist.make(f"s3://{bucket}/{key}")
         else:  # pragma: no cover
             raise e
+
+
+def is_object_exists(
+    s3_client: "S3Client",
+    bucket: str,
+    key: str,
+    version_id: str = NOTHING,
+) -> bool:
+    """
+    Check if an object exists. If you want to use the head_object_ API response
+    immediately when the object exists, use :func:`head_object` instead.
+
+    Example::
+
+        >>> is_object_exists(s3_client, "my-bucket", "file.txt")
+        True
+
+    :param s3_client: See head_object_
+    :param bucket: See head_object_
+    :param key: See head_object_
+    :param version_id: See head_object_
+
+    :return: A Boolean flag to indicate whether the object exists.
+    """
+    response = head_object(
+        s3_client=s3_client,
+        bucket=bucket,
+        key=key,
+        version_id=version_id,
+        ignore_not_found=True,
+    )
+    if response is None:
+        return False
+    else:
+        return True
