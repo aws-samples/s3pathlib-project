7a6bb978c7a24020edeba6199944c2730ea32f59
Sanhe Hu <husanhe@gmail.com>
2023-04-16 23:45:11 -0400

    Update test_core_copy.py

--- tests/core/test_core_copy.py
+++ tests/core/test_core_copy.py
@@ -3,18 +3,18 @@
 import pytest
 from pathlib_mate import Path
 from s3pathlib.core import S3Path
-from s3pathlib.tests import bucket, prefix, run_cov_test
-
+from s3pathlib.tests import run_cov_test
+from s3pathlib.tests.mock import BaseTest
 
 dir_here = Path.dir_here(__file__)
 
-s3dir_root = S3Path(bucket, prefix, "core", "copy").to_dir()
 
+class CopyAPIMixin(BaseTest):
+    module = "core.copy"
 
-class TestCopyAPIMixin:
-    def test_copy_object(self):
+    def _test_copy_object(self):
         # before state
-        p_src = S3Path(s3dir_root, "copy-object", "before.py")
+        p_src = S3Path(self.s3dir_root, "copy-object", "before.py")
         p_src.write_text("a")
 
         p_dst = S3Path(p_src, "copy-object", "after.py")
@@ -33,9 +33,9 @@ class TestCopyAPIMixin:
         with pytest.raises(FileExistsError):
             p_src.copy_to(p_dst, overwrite=False)
 
-    def test_copy_dir(self):
+    def _test_copy_dir(self):
         # before state
-        p_src = S3Path(s3dir_root, "copy-dir", "before").to_dir()
+        p_src = S3Path(self.s3dir_root, "copy-dir", "before").to_dir()
         p_src.delete_if_exists()
         assert p_src.count_objects() == 0
 
@@ -46,7 +46,7 @@ class TestCopyAPIMixin:
             overwrite=True,
         )
 
-        p_dst = S3Path(s3dir_root, "copy-dir", "after").to_dir()
+        p_dst = S3Path(self.s3dir_root, "copy-dir", "after").to_dir()
         p_dst.delete_if_exists()
         assert p_dst.count_objects() == 0
 
@@ -60,9 +60,9 @@ class TestCopyAPIMixin:
         with pytest.raises(FileExistsError):
             p_src.copy_to(dst=p_dst, overwrite=False)
 
-    def test_move_to(self):
+    def _test_move_to(self):
         # before state
-        p_src = S3Path(s3dir_root, "move-to", "before").to_dir()
+        p_src = S3Path(self.s3dir_root, "move-to", "before").to_dir()
         p_src.delete_if_exists()
         assert p_src.count_objects() == 0
 
@@ -73,7 +73,7 @@ class TestCopyAPIMixin:
             overwrite=True,
         )
 
-        p_dst = S3Path(s3dir_root, "move-to", "after/")
+        p_dst = S3Path(self.s3dir_root, "move-to", "after/")
         p_dst.delete_if_exists()
 
         assert p_dst.count_objects() == 0
@@ -85,6 +85,19 @@ class TestCopyAPIMixin:
         assert count == 2
         assert p_src.count_objects() == 0
         assert p_dst.count_objects() == 2
+
+    def test(self):
+        self._test_copy_object()
+        self._test_copy_dir()
+        self._test_move_to()
+
+
+class Test(CopyAPIMixin):
+    use_mock = False
+
+
+class TestWithVersioning(CopyAPIMixin):
+    use_mock = True
 
 
 if __name__ == "__main__":
