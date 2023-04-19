ef24372158f806cf2c0e1b77ef92d05b65739fb2
Sanhe Hu <husanhe@gmail.com>
2023-04-18 23:46:40 -0400

    add full list documents to read_bytes and read_text

--- s3pathlib/core/rw.py
+++ s3pathlib/core/rw.py
@@ -3,7 +3,11 @@
 """
 Read and write related API.
 
+.. _bsm: https://github.com/aws-samples/boto-session-manager-project
+.. _get_object: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html#S3.Client.get_object
 .. _put_object: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html#S3.Client.put_object
+.. _decode: https://docs.python.org/3/library/stdtypes.html#bytes.decode
+.. _encode: https://docs.python.org/3/library/stdtypes.html#str.encode
 """
 
 import typing as T
@@ -30,23 +34,87 @@ class ReadAndWriteAPIMixin:
     """
     A mixin class that implements the Text / Bytes, Read / Write methods.
     """
-
     def read_bytes(
         self: "S3Path",
+        version_id: str = NOTHING,
+        if_match: str = NOTHING,
+        if_modified_since: datetime = NOTHING,
+        if_none_match: str = NOTHING,
+        if_unmodified_since: datetime = NOTHING,
+        range: str = NOTHING,
+        response_cache_control: str = NOTHING,
+        response_content_disposition: str = NOTHING,
+        response_content_encoding: str = NOTHING,
+        response_content_language: str = NOTHING,
+        response_content_type: str = NOTHING,
+        response_expires: str = NOTHING,
+        sse_customer_algorithm: str = NOTHING,
+        sse_customer_key: str = NOTHING,
+        request_payer: str = NOTHING,
+        part_number: int = NOTHING,
+        expected_bucket_owner: str = NOTHING,
+        checksum_mode: str = NOTHING,
         bsm: T.Optional["BotoSesManager"] = None,
     ) -> bytes:
         """
-        Read binary data from s3 object. A simple wrapper around
-        `s3_client.get_object <https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html#S3.Client.get_object>`_
+        Read binary data from s3 object.
+        A simple wrapper around get_object_.
+
+        :param version_id: See get_object_.
+        :param if_match: See get_object_.
+        :param if_modified_since: See get_object_.
+        :param if_none_match: See get_object_.
+        :param if_unmodified_since: See get_object_.
+        :param range: See get_object_.
+        :param response_cache_control: See get_object_.
+        :param response_content_disposition: See get_object_.
+        :param response_content_encoding: See get_object_.
+        :param response_content_language: See get_object_.
+        :param response_content_type: See get_object_.
+        :param response_expires: See get_object_.
+        :param sse_customer_algorithm: See get_object_.
+        :param sse_customer_key: See get_object_.
+        :param request_payer: See get_object_.
+        :param part_number: See get_object_.
+        :param expected_bucket_owner: See get_object_.
+        :param checksum_mode: See get_object_.
+        :param bsm: See bsm_.
+
+        .. versionadded:: 1.0.3
 
         .. versionchanged:: 1.1.2
 
             automatically store metadata in cache.
+            
+        .. versionchanged:: 2.1.1
+        
+            add ``version_id`` parameter, and now support full list of get_object_
+            arguments.
         """
         s3_client = resolve_s3_client(context, bsm)
         response = s3_client.get_object(
-            Bucket=self.bucket,
-            Key=self.key,
+            **resolve_kwargs(
+                Bucket=self.bucket,
+                Key=self.key,
+                VersionId=version_id,
+                IfMatch=if_match,
+                IfModifiedSince=if_modified_since,
+                IfNoneMatch=if_none_match,
+                IfUnmodifiedSince=if_unmodified_since,
+                Range=range,
+                ResponseCacheControl=response_cache_control,
+                ResponseContentDisposition=response_content_disposition,
+                ResponseContentEncoding=response_content_encoding,
+                ResponseContentLanguage=response_content_language,
+                ResponseContentType=response_content_type,
+                ResponseExpires=response_expires,
+                SSECustomerAlgorithm=sse_customer_algorithm,
+                SSECustomerKey=sse_customer_key,
+                RequestPayer=request_payer,
+                PartNumber=part_number,
+                ExpectedBucketOwner=expected_bucket_owner,
+                ChecksumMode=checksum_mode,
+            ),
         )
         data = response["Body"].read()
         del response["Body"]
@@ -58,18 +126,84 @@ class ReadAndWriteAPIMixin:
         self: "S3Path",
         encoding="utf-8",
         errors="strict",
+        version_id: str = NOTHING,
+        if_match: str = NOTHING,
+        if_modified_since: datetime = NOTHING,
+        if_none_match: str = NOTHING,
+        if_unmodified_since: datetime = NOTHING,
+        range: str = NOTHING,
+        response_cache_control: str = NOTHING,
+        response_content_disposition: str = NOTHING,
+        response_content_encoding: str = NOTHING,
+        response_content_language: str = NOTHING,
+        response_content_type: str = NOTHING,
+        response_expires: str = NOTHING,
+        sse_customer_algorithm: str = NOTHING,
+        sse_customer_key: str = NOTHING,
+        request_payer: str = NOTHING,
+        part_number: int = NOTHING,
+        expected_bucket_owner: str = NOTHING,
+        checksum_mode: str = NOTHING,
         bsm: T.Optional["BotoSesManager"] = None,
     ) -> str:
         """
-        Read text data from s3 object. A simple wrapper around s3_client.get_object_
+        Read text data from s3 object.
+        A simple wrapper around get_object_.
+
+        :param encoding: See decode_.
+        :param errors: See decode_.
+        :param version_id: See get_object_.
+        :param if_match: See get_object_.
+        :param if_modified_since: See get_object_.
+        :param if_none_match: See get_object_.
+        :param if_unmodified_since: See get_object_.
+        :param range: See get_object_.
+        :param response_cache_control: See get_object_.
+        :param response_content_disposition: See get_object_.
+        :param response_content_encoding: See get_object_.
+        :param response_content_language: See get_object_.
+        :param response_content_type: See get_object_.
+        :param response_expires: See get_object_.
+        :param sse_customer_algorithm: See get_object_.
+        :param sse_customer_key: See get_object_.
+        :param request_payer: See get_object_.
+        :param part_number: See get_object_.
+        :param expected_bucket_owner: See get_object_.
+        :param checksum_mode: See get_object_.
+        :param bsm: See bsm_.
+
+        .. versionadded:: 1.0.3
 
         .. versionchanged:: 1.1.2
 
             automatically store metadata in cache.
 
-        .. _get_object: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html#S3.Client.get_object
+        .. versionchanged:: 2.1.1
+
+            add ``version_id`` parameter, and now support full list of get_object_
+            arguments.
         """
-        data = self.read_bytes(bsm=bsm)
+        data = self.read_bytes(
+            version_id=version_id,
+            if_match=if_match,
+            if_modified_since=if_modified_since,
+            if_none_match=if_none_match,
+            if_unmodified_since=if_unmodified_since,
+            range=range,
+            response_cache_control=response_cache_control,
+            response_content_disposition=response_content_disposition,
+            response_content_encoding=response_content_encoding,
+            response_content_language=response_content_language,
+            response_content_type=response_content_type,
+            response_expires=response_expires,
+            sse_customer_algorithm=sse_customer_algorithm,
+            sse_customer_key=sse_customer_key,
+            request_payer=request_payer,
+            part_number=part_number,
+            expected_bucket_owner=expected_bucket_owner,
+            checksum_mode=checksum_mode,
+            bsm=bsm,
+        )
         return data.decode(encoding, errors=errors)
 
     def write_bytes(
@@ -77,7 +211,6 @@ class ReadAndWriteAPIMixin:
         data: bytes,
         metadata: MetadataType = NOTHING,
         tags: TagType = NOTHING,
-        bsm: T.Optional["BotoSesManager"] = None,
         acl: str = NOTHING,
         cache_control: str = NOTHING,
         content_disposition: str = NOTHING,
@@ -109,15 +242,15 @@ class ReadAndWriteAPIMixin:
         object_lock_retain_until_datetime: datetime = NOTHING,
         object_lock_legal_hold_status: str = NOTHING,
         expected_bucket_owner: str = NOTHING,
+        bsm: T.Optional["BotoSesManager"] = None,
     ) -> "PutObjectOutputTypeDef":
         """
-        Write binary data to s3 object. A simple wrapper around
-        `s3_client.put_object <https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html#S3.Client.put_object>`_
+        Write binary data to s3 object.
+        A simple wrapper around put_object_.
 
         :param data: the text you want to write.
         :param metadata: the s3 object metadata in string key value pair dict.
         :param tags: the s3 object tags in string key value pair dict.
-        :param bsm: ``boto_session_manager.BotoSesManager`` object.
         :param acl: See put_object_.
         :param cache_control: See put_object_.
         :param content_disposition: See put_object_.
@@ -149,12 +282,13 @@ class ReadAndWriteAPIMixin:
         :param object_lock_retain_until_datetime: See put_object_.
         :param object_lock_legal_hold_status: See put_object_.
         :param expected_bucket_owner: See put_object_.
+        :param bsm: See bsm_.
 
         .. versionadded:: 1.0.3
 
         .. versionchanged:: 1.1.1
 
-            allow update metadata and tags as well
+            add ``metadata`` and ``tags`` parameters.
         """
         s3_client = resolve_s3_client(context, bsm)
         response = s3_client.put_object(
@@ -209,7 +343,6 @@ class ReadAndWriteAPIMixin:
         errors: str = "strict",
         metadata: MetadataType = NOTHING,
         tags: TagType = NOTHING,
-        bsm: T.Optional["BotoSesManager"] = None,
         acl: str = NOTHING,
         cache_control: str = NOTHING,
         content_disposition: str = NOTHING,
@@ -241,24 +374,55 @@ class ReadAndWriteAPIMixin:
         object_lock_retain_until_datetime: datetime = NOTHING,
         object_lock_legal_hold_status: str = NOTHING,
         expected_bucket_owner: str = NOTHING,
+        bsm: T.Optional["BotoSesManager"] = None,
     ) -> "PutObjectOutputTypeDef":
         """
-        Write text to s3 object. A simple wrapper around
-        `s3_client.put_object <https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html#S3.Client.put_object>`_
+        Write text to s3 object.
+        A simple wrapper around put_object_.
 
         :param data: the text you want to write.
-        :param encoding: how do you want to encode text?
-        :param errors: how do you want to handle encode error? can be 'strict',
-            ``ignore``, ``replace``, ``xmlcharrefreplace``, ``backslashreplace``.
-            see more details `here <https://docs.python.org/3/library/stdtypes.html#str.encode>`_.
+        :param encoding: See encode_.
+        :param errors: See encode_.
         :param metadata: the s3 object metadata in string key value pair dict.
         :param tags: the s3 object tags in string key value pair dict.
+        :param acl: See put_object_.
+        :param cache_control: See put_object_.
+        :param content_disposition: See put_object_.
+        :param content_encoding: See put_object_.
+        :param content_language: See put_object_.
+        :param content_length: See put_object_.
+        :param content_md5: See put_object_.
+        :param content_type: See put_object_.
+        :param checksum_algorithm: See put_object_.
+        :param checksum_crc32: See put_object_.
+        :param checksum_crc32c: See put_object_.
+        :param checksum_sha1: See put_object_.
+        :param checksum_sha256: See put_object_.
+        :param expires_datetime: See put_object_.
+        :param grant_full_control: See put_object_.
+        :param grant_read: See put_object_.
+        :param grant_read_acp: See put_object_.
+        :param grant_write_acp: See put_object_.
+        :param server_side_encryption: See put_object_.
+        :param storage_class: See put_object_.
+        :param website_redirect_location: See put_object_.
+        :param sse_customer_algorithm: See put_object_.
+        :param sse_customer_key: See put_object_.
+        :param sse_kms_key_id: See put_object_.
+        :param sse_kms_encryption_context: See put_object_.
+        :param bucket_key_enabled: See put_object_.
+        :param request_payer: See put_object_.
+        :param object_lock_mode: See put_object_.
+        :param object_lock_retain_until_datetime: See put_object_.
+        :param object_lock_legal_hold_status: See put_object_.
+        :param expected_bucket_owner: See put_object_.
+        :param bsm: See bsm_.
 
         .. versionadded:: 1.0.3
 
         .. versionchanged:: 1.1.1
 
-            allow update metadata and tags as well
+            add ``metadata`` and ``tags`` parameters.
         """
         body = data.encode(encoding, errors=errors)
         return self.write_bytes(
@@ -344,9 +508,47 @@ class ReadAndWriteAPIMixin:
         :param exist_ok: if True, it won't raise error when the S3 object
             already exists.
 
-        .. versionchanged:: 1.0.6
+        :param data: the text you want to write.
+        :param metadata: the s3 object metadata in string key value pair dict.
+        :param tags: the s3 object tags in string key value pair dict.
+        :param acl: See put_object_.
+        :param cache_control: See put_object_.
+        :param content_disposition: See put_object_.
+        :param content_encoding: See put_object_.
+        :param content_language: See put_object_.
+        :param content_length: See put_object_.
+        :param content_md5: See put_object_.
+        :param content_type: See put_object_.
+        :param checksum_algorithm: See put_object_.
+        :param checksum_crc32: See put_object_.
+        :param checksum_crc32c: See put_object_.
+        :param checksum_sha1: See put_object_.
+        :param checksum_sha256: See put_object_.
+        :param expires_datetime: See put_object_.
+        :param grant_full_control: See put_object_.
+        :param grant_read: See put_object_.
+        :param grant_read_acp: See put_object_.
+        :param grant_write_acp: See put_object_.
+        :param server_side_encryption: See put_object_.
+        :param storage_class: See put_object_.
+        :param website_redirect_location: See put_object_.
+        :param sse_customer_algorithm: See put_object_.
+        :param sse_customer_key: See put_object_.
+        :param sse_kms_key_id: See put_object_.
+        :param sse_kms_encryption_context: See put_object_.
+        :param bucket_key_enabled: See put_object_.
+        :param request_payer: See put_object_.
+        :param object_lock_mode: See put_object_.
+        :param object_lock_retain_until_datetime: See put_object_.
+        :param object_lock_legal_hold_status: See put_object_.
+        :param expected_bucket_owner: See put_object_.
+        :param bsm: See bsm_.
+
+        .. versionadded:: 1.0.6
 
         .. versionchanged:: 1.2.1
+
+            add ``metadata`` and ``tags`` parameters.
         """
         self.ensure_object()
 
@@ -403,7 +605,7 @@ class ReadAndWriteAPIMixin:
         """
         Make an S3 folder (empty "/" file)
 
-        .. versionchanged:: 1.0.6
+        .. versionadded:: 1.0.6
         """
         if not self.is_dir():
             raise ValueError(f"{self.uri} is not a directory, you cannot make dir!")
