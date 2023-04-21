3e297045c39b87067e22818fc2dbb08303c705ec
Sanhe Hu <husanhe@gmail.com>
2023-04-21 15:49:53 -0400

    update CI

--- setup.py
+++ setup.py
@@ -135,7 +135,6 @@ if __name__ == "__main__":
         "Operating System :: Unix",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
